462b262dd1cd35dc83d73b8ec386b14783a7dfd6
Destiny <zhukng@163.com>
2024-01-28 13:58:24 +0800

    feat: 初始化日志

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"snake/pkg"
+)
+
+func main() {
+	// Initialize Log
+	pkg.Logger.Info("Logger initialized")
+
+	// Initialize Register Center
+
+	// Load the Config
+
+	// Initialize Database
+
+	// Initialize Middleware
+}
